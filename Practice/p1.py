4ffa2888b0aa9606495f5e5dc57f1a0d472e2437
Sarchit007 <sarchit971@gmail.com>
2026-03-12 21:09:26 +0530

    first commit

---
+++ Practice/p1.py
@@ -0,0 +1,37 @@
+from time import sleep
+
+# task 1
+# from selenium.webdriver import Chrome,ChromeOptions
+# o = ChromeOptions()
+# o.add_experimental_option("detach", True)
+# driver = Chrome(options=o)
+# driver.get("https://www.wikipedia.org/")
+# print(driver.title)
+
+
+# task 2
+# from selenium.webdriver import Chrome,ChromeOptions
+# o = ChromeOptions()
+# o.add_experimental_option("detach", True)
+# driver = Chrome(options=o)
+# driver.get("https://www.wikipedia.org/")
+# print(driver.current_url)
+
+
+
+
+# task 3
+# from selenium.webdriver import Chrome,ChromeOptions
+# o = ChromeOptions()
+# o.add_experimental_option("detach", True)
+# driver = Chrome(options=o)
+# driver.get("https://www.wikipedia.org/")
+# driver.refresh()
+# print(driver.title)
+# sleep(3)
+# driver.get("https://www.amazon.in/")
+# sleep(3)
+# print(driver.title)
+# driver.back()
+# sleep(3)
+# driver.close()
